9447f4604e5e1b99561316a91a4be616bd54b39f
hanyu <531868219@qq.com>
2019-12-18 11:30:42 +0800

    init project

---
+++ demo.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/12/18 0018
+ * Time: 11:29
+ */
+require './vendor/autoload.php';
+
+use Ihanyv\DompdfChTool\Tools;
+
+Tools::Hello();
