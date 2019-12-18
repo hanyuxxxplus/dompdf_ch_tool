46b311e24082ffceda945e9d86fc26edb3ceb457
hanyu <531868219@qq.com>
2019-12-18 15:20:51 +0800

    test project

--- src/Ihanyv/Execute.php
+++ src/Ihanyv/Execute.php
@@ -25,4 +25,29 @@ class Execute
             system('php ' . $loadPhp . $font);
         }
     }
+
+    public function ch_text()
+    {
+        // ihanyv 目录
+        $ihanyv = dirname(dirname(__FILE__));
+        // src 目录
+        $src = dirname($ihanyv);
+
+        // dompdf-ch-tool 目录
+        $dompdf = dirname($src);
+
+        // ihanyv 目录
+        $ihanyvS = dirname($dompdf);
+
+        // vendor 目录
+        $vendor = dirname($ihanyvS);
+
+        //dompdf\dompdf\src\FrameReflower
+        // 本地text.php 目录
+        $local = $ihanyv. '/DompdfChTool/Text.php';
+        // text.php 目录
+        $target = $vendor.'/dompdf/dompdf/src/FrameReflower/Text.php';
+
+        system("cp $local $target");
+    }
 }
